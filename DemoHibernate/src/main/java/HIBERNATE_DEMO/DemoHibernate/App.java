2d0702300ffa001ed1e518bcebb6014dafe1326d
Sarveshltts <78848824+Sarveshltts@users.noreply.github.com>
2021-03-12 12:14:12 +0530

    Add files via upload

---
+++ DemoHibernate/src/main/java/HIBERNATE_DEMO/DemoHibernate/App.java
@@ -0,0 +1,33 @@
+package HIBERNATE_DEMO.DemoHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration cfg=new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory=cfg.buildSessionFactory();
+        Session session=factory.openSession();
+        Transaction t=session.beginTransaction();
+        Employee e= new Employee();
+        e.setEmpid(101);
+        e.setEmpname("Sarvesh");
+        e.setEmpaddress("Agra");
+        session.save(e);
+        t.commit();
+       
+        factory.close();
+        session.close();
+    }
+}
